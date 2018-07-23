b0b1f17794a2ee39d33fe38a3ba2d234178d3b4d
baidoosik <qoentlr37@naver.com>
2018-07-23 15:40:59 +0900

    first commit

---
+++ DocumentDBGettingStarted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentDBGettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
